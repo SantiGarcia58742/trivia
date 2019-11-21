3b54f382c376cb23d6eb2d390c25df437cba4d01
Santiago Garcia <santigar02@hotmail.com.ar>
2019-11-21 17:24:06 -0300

    cosas

--- index.js
+++ index.js
@@ -1,1 +1,102 @@
-var app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+//bd
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log(`DB connected @ ${db}`);
+  })
+.catch(err => console.error(`Connection error ${err}`));
+//vistas
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+const QuestionSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  answers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Answer' }]
+});
+
+const Question = mongoose.model('Question', QuestionSchema);
+//ruta
+app.post('/question', (req, res) => {
+  let question = new Question({
+    _id: new mongoose.Types.ObjectId(),
+    title: req.body.question
+  });
+  let answerA = new Answer({
+    title: req.body.answerA,
+    isRight: req.body.checkboxA,
+    question: question._id
+  });
+  let answerB = new Answer({
+    title: req.body.answerB,
+    isRight: req.body.checkboxB,
+    question: question._id
+  });
+  let answerC = new Answer({
+    title: req.body.answerC,
+    isRight: req.body.checkboxC,
+    question: question._id
+  });
+  let answerD = new Answer({
+    title: req.body.answerD,
+    isRight: req.body.checkboxD,
+    question: question._id
+  });
+  question.answers.push(answerA);
+  question.answers.push(answerB);
+  question.answers.push(answerC);
+  question.answers.push(answerD);
+  question.save(err => {
+    answerA.save();
+    answerB.save();
+    answerC.save();
+    answerD.save();
+    res.redirect('/');
+  });
+});
+
+app.get('/', (req, res) => {
+  Question.find().populate('answers').exec((err, questions) => {
+    res.render('index', { questions: questions, user: req.user });
+  });
+});
+//ruta sign up
+app.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+
+app.post('/signup', (req, res, next) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  // hashear password
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) throw err;
+      user.password = hash;
+      user.save(err => {
+        if (err) return next(err);
+        res.redirect('/');
+      });
+    });
+  });
+});
+//ruta log in
+app.post('/login',
+  passport.authenticate('local', { failureRedirect:
+  '/' }),
+  (req, res, next) => {
+    res.redirect('/');
+});
+//ruta log out
+app.get('/logout',
+  (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
